1490570fac83b09f79dbd8d46d2df7a529739307
Md Muhir Uddin <mdmuhir03@gmail.com>
2024-12-13 20:13:13 +0600

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -22,12 +22,12 @@ onBeforeMount(async () => {
     <div class="body mt-24 sm:mt-16 dark:bg-[#212121]">
       <div
         v-if="product.toggleFilteredProducts()"
-        class="Filtered-Profducts fixed top-0 left-0 right-0 bottom-0 mt-[8rem] sm:mt-[65px] z-40 bg-stone-800/85 dark:bg-gray-800/95 w-full h-full py-2 rounded-lg shadow-inner shadow-cyan-500"
+        class="Filtered-Profducts fixed top-0 left-0 right-0 bottom-0 mt-[7rem] sm:mt-[65px] z-40 bg-stone-800/85 dark:bg-gray-800/95 w-full min-h-svh sm:pt-2 pb-32  shadow-inner shadow-cyan-500 overflow-y-auto"
       >
         <h2 class="text-cyan-400 bg-black w-36 text-center px-2 border">
           Filtered Products
         </h2>
-        <ProductsList :filteredProducts="product.filteredProducts" class=" h-auto overflow-scroll" />
+        <ProductsList :filteredProducts="product.filteredProducts" class=" h-auto" />
       </div>
       <slot />
     </div>
