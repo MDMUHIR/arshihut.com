e4b6866c967a2bf88256346989d9f8bab1f055f6
Md Muhir Uddin <mdmuhir03@gmail.com>
2025-01-15 08:53:00 +0600

    Update Products.vue

--- pages/Products.vue
+++ pages/Products.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <div class="main py-5 min-h-svh bg-stone-100 dark:bg-[#212121] xl:container xl:mx-auto">
+  <div class="main py-12 min-h-svh bg-stone-100 dark:bg-[#212121] xl:container xl:mx-auto">
     <!-- <pre>{{ auth.getUserToken() }}</pre> -->
     <ProductsList :filteredProducts="data.products"  />
   </div>
