c31203016406e8f319d71ba6c2d76978b5ec0d28
Md Muhir Uddin <mdmuhir03@gmail.com>
2025-01-15 08:40:56 +0600

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   css: ["~/assets/css/main.css"],
   app: {
     head: {
-      title: "MD MUHIR",
+      title: "DIGITAL NEST",
       link: [{ rel: "icon", type: "image/png", href: "/favicon.png" }],
     },
   },
